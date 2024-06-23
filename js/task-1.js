8ca0ee0be9b59c40f199c8adb040046aa6ae3f48
Viktor S <sandorikviktor@gmail.com>
2024-06-23 19:29:25 +0300

    tsk1.2

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,9 @@
-const categories = document.querySelectorAll(".item");
-console.log(`Number of categories: ${categories.length}`);
-
-const categoriesData = categories.forEach((category) => {
-  const categoryTitle = category.querySelector("h2").textContent;
-  console.log(`Category: ${categoryTitle}`);
-
-  const categoriesQuantity = category.querySelectorAll("li").length;
-  console.log(`Elements: ${categoriesQuantity}`);
-});
+const listItem = document.querySelectorAll("#categories > .item");
+console.log("Number of categories: ", listItem.length);
+const itemTitles = document.querySelectorAll("#categories > .item > h2");
+const nestedUlElements = document.querySelectorAll("#categories > .item > ul");
+for (let i = 0; i <= listItem.length - 1; i += 1) {
+  console.log("Category: ", itemTitles[i].textContent);
+  const liElementsInNestedUl = nestedUlElements[i].querySelectorAll("li");
+  console.log("Elements: ", liElementsInNestedUl.length);
+}
